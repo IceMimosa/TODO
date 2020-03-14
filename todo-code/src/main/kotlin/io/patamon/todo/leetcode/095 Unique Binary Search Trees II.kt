227e1043546c7e52e0602b4aa1126b256feb80ae
IceMimosa <chk19940609@gmail.com>
2020-03-14 22:46:41 +0800

    [LeetCode] 98. Validate Binary Search Tree

---
+++ todo-code/src/main/kotlin/io/patamon/todo/leetcode/095 Unique Binary Search Trees II.kt
@@ -0,0 +1,20 @@
+package io.patamon.todo.leetcode
+
+/**
+ * Desc:
+ *
+ * 95. Unique Binary Search Trees II (不同的二叉搜索树 II)
+ *
+ * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的二叉搜索树。
+ *
+ * Mail: chk19940609@gmail.com
+ * Created by IceMimosa
+ * Date: 2020-03-14
+ */
+
+
+/*
+
+TODO: https://leetcode-cn.com/problems/unique-binary-search-trees-ii/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-2-7/
+
+ */
---
+++ todo-code/src/main/kotlin/io/patamon/todo/leetcode/098 Validate Binary Search Tree.kt
@@ -0,0 +1,52 @@
+package io.patamon.todo.leetcode
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.patamon.todo.common.TreeNode
+
+/**
+ * Desc:
+ *
+ * # 98. Validate Binary Search Tree (验证二叉搜索树)
+ *
+ * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+ *
+ * 假设一个二叉搜索树具有如下特征：
+ * * 节点的左子树只包含小于当前节点的数。
+ * * 节点的右子树只包含大于当前节点的数。
+ * * 所有左子树和右子树自身必须也是二叉搜索树。
+ *
+ *
+ * Mail: chk19940609@gmail.com
+ * Created by IceMimosa
+ * Date: 2020-03-14
+ */
+
+class ValidateBinarySearchTree : StringSpec({
+
+
+    "Validate Binary Search Tree" {
+        TreeNode(2, 1, 3).isValidBST(null, null) shouldBe true
+        TreeNode(5, 1, null)
+                .right(TreeNode(4, 3, 6))
+                .isValidBST(null, null) shouldBe false
+    }
+
+})
+
+/**
+ * 递归, 设置左右上限
+ */
+private fun TreeNode?.isValidBST(lower: Int?, upper: Int?): Boolean {
+    this ?: return true
+
+    if (lower != null && this.`val` <= lower) return false
+    if (upper != null && this.`val` >= upper) return false
+
+    return this.left.isValidBST(lower, this.`val`)
+            && this.right.isValidBST(this.`val`, upper)
+}
+
+/**
+ * 中序遍历, 一遍搞定
+ */
