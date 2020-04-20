fc8d3957f67b1279e5261616ea1cea87ecdb4e08
IceMimosa <chk19940609@gmail.com>
2020-04-21 00:35:56 +0800

    [LeetCode] Add some codes

---
+++ todo-code/src/main/kotlin/io/patamon/todo/leetcode/108 Convert Sorted Array to Binary Search Tree.kt
@@ -0,0 +1,46 @@
+package io.patamon.todo.leetcode
+
+import io.kotlintest.specs.StringSpec
+import io.patamon.todo.common.TreeNode
+
+/**
+ * Desc:
+ *  108. Convert Sorted Array to Binary Search Tree (将有序数组转换为二叉搜索树)
+ *
+ * 将一个按照升序排列的有序数组，转换为一棵高度平衡二叉搜索树。
+ * 本题中，一个高度平衡二叉树是指一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过 1。
+ *
+ * Mail: chk19940609@gmail.com
+ * Created by IceMimosa
+ * Date: 2020-04-20
+ */
+class ConvertSortedArrayToBinarySearchTree : StringSpec({
+
+    "Convert Sorted Array to Binary Search Tree" {
+        intArrayOf(-10, -3, 0, 5, 9).sortedArrayToBST()
+    }
+
+})
+
+/**
+ * 根据 中序遍历 数据去恢复一颗平衡二叉树, 那么折半恢复即可
+ *
+ * 其他根节点选择: https://leetcode-cn.com/problems/convert-sorted-array-to-binary-search-tree/solution/jiang-you-xu-shu-zu-zhuan-huan-wei-er-cha-sou-s-15/
+ */
+private fun IntArray?.sortedArrayToBST(): TreeNode? {
+    if (this == null || this.isEmpty()) return null
+
+    val mid = this.size / 2
+    val node = TreeNode(this[mid])
+
+    // 左边到头
+    if (mid > 0) {
+        node.left = this.sliceArray(0 until mid).sortedArrayToBST()
+    }
+
+    // 右边到头
+    if (mid < this.size - 1) {
+        node.right = this.sliceArray(mid + 1 until this.size).sortedArrayToBST()
+    }
+    return node
+}
---
+++ todo-code/src/main/kotlin/io/patamon/todo/leetcode/110 Balanced Binary Tree.kt
@@ -0,0 +1,66 @@
+package io.patamon.todo.leetcode
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.patamon.todo.common.TreeNode
+
+/**
+ * Desc:
+ *  110. Balanced Binary Tree (平衡二叉树)
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ *
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ *
+ *
+ * https://leetcode-cn.com/problems/balanced-binary-tree/solution/ping-heng-er-cha-shu-by-leetcode/
+ *
+ * Mail: chk19940609@gmail.com
+ * Created by IceMimosa
+ * Date: 2020-04-21
+ */
+
+class BalancedBinaryTree : StringSpec({
+
+
+    "Balanced Binary Tree" {
+        TreeNode(1, null)
+                .right(TreeNode(2, null, 3))
+                .isBalanced() shouldBe false
+        TreeNode(3)
+                .left(9)
+                .right(TreeNode(20, 15, 7))
+                .isBalanced() shouldBe true
+        TreeNode(1)
+                .right(2)
+                .left(TreeNode(2).right(3).left(TreeNode(3, 4, 4)))
+                .isBalanced() shouldBe false
+    }
+
+})
+
+/**
+ * 自顶向下递归, 只要有一个节点满足  |height(p.left) - height(p.right)| > 1 时, 它就是不平衡的
+ *
+ * 时间复杂度: O(nlogn)
+ * 空间复杂度: O(n)
+ */
+private fun TreeNode?.isBalanced(): Boolean {
+    this ?: return true
+
+    val l = this.left.level()
+    val r = this.right.level()
+
+    return (Math.abs(l - r) <= 1) && this.left.isBalanced() && this.right.isBalanced()
+}
+
+// 获取树的高度
+private fun TreeNode?.level(l: Int = 0): Int {
+    this ?: return l
+    return Math.max(this.left.level(l + 1), this.right.level(l + 1))
+}
+
+/**
+ * 也可以使用 自底向上 的方式, 后序遍历的一种应用
+ */
