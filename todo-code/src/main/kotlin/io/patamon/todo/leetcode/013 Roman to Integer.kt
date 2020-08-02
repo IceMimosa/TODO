d8e4938c279cf3c9a5801b13590791bc42c16516
IceMimosa <chk19940609@gmail.com>
2020-08-02 19:56:44 +0800

    Add some

---
+++ todo-code/src/main/kotlin/io/patamon/todo/leetcode/013 Roman to Integer.kt
@@ -0,0 +1,93 @@
+package io.patamon.todo.leetcode
+
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+
+/**
+ * Desc:
+ *  13. Roman to Integer (罗马数字转整数)
+ *
+ * 罗马数字包含以下七种字符: I， V， X， L，C，D 和 M。
+ *
+ * 字符          数值
+ * I             1
+ * V             5
+ * X             10
+ * L             50
+ * C             100
+ * D             500
+ * M             1000
+ * 例如， 罗马数字 2 写做 II ，即为两个并列的 1。12 写做 XII ，即为 X + II 。 27 写做  XXVII, 即为 XX + V + II 。
+ *
+ * 通常情况下，罗马数字中小的数字在大的数字的右边。但也存在特例，例如 4 不写做 IIII，而是 IV。数字 1 在数字 5 的左边，所表示的数等于大数 5 减小数 1 得到的数值 4 。同样地，数字 9 表示为 IX。这个特殊的规则只适用于以下六种情况：
+ *   I 可以放在 V (5) 和 X (10) 的左边，来表示 4 和 9。
+ *   X 可以放在 L (50) 和 C (100) 的左边，来表示 40 和 90。 
+ *   C 可以放在 D (500) 和 M (1000) 的左边，来表示 400 和 900。
+ * 给定一个罗马数字，将其转换成整数。输入确保在 1 到 3999 的范围内。
+ *
+ * 示例 1:
+ * 输入: "III"
+ * 输出: 3
+ *
+ * 示例 2:
+ * 输入: "IV"
+ * 输出: 4
+ *
+ * 示例 3:
+ * 输入: "IX"
+ * 输出: 9
+ *
+ * 示例 4:
+ * 输入: "LVIII"
+ * 输出: 58
+ * 解释: L = 50, V= 5, III = 3.
+ *
+ * 示例 5:
+ * 输入: "MCMXCIV"
+ * 输出: 1994
+ * 解释: M = 1000, CM = 900, XC = 90, IV = 4.
+ *
+ * Mail: chk19940609@gmail.com
+ * Created by IceMimosa
+ * Date: 2020/8/2
+ */
+class RomanToInteger : StringSpec({
+
+    "Roman to Integer" {
+        "III".romanToInt() shouldBe 3
+        "IV".romanToInt() shouldBe 4
+        "IX".romanToInt() shouldBe 9
+        "LVIII".romanToInt() shouldBe 58
+        "MCMXCIV".romanToInt() shouldBe 1994
+    }
+})
+
+private fun String?.romanToInt(): Int {
+    if (this == null || this.isEmpty()) return 0
+    // lambda
+    val getNum = { c: Char ->
+        when (c) {
+            'I' -> 1
+            'V' -> 5
+            'X' -> 10
+            'L' -> 50
+            'C' -> 100
+            'D' -> 500
+            'M' -> 1000
+            else -> 0
+        }
+    }
+
+    var postNum = getNum(this[this.length - 1])
+    var res = postNum
+    (this.length - 2 downTo 0).forEach { i ->
+        val currNum = getNum(this[i])
+        if (currNum >= postNum) {
+            res += currNum
+        } else {
+            res -= currNum
+        }
+        postNum = currNum
+    }
+    return res
+}
